6ed74f09a12abef9121d15170921bdd679d33e25
Matt Packwood <mattpackwood@gmail.com>
2021-05-26 16:17:23 -0400

    Update test1.py
    Updating and testing...

--- test1.py
+++ test1.py
@@ -13,7 +13,7 @@ apobj = apprise.Apprise()
 
 # Add all of the notification services by their server url.
 # A sample IFTTT notification  - Working 100%
-# apobj.add('ifttt://'+secrets['IFTT_key'])
+apobj.add('ifttt://'+secrets['IFTTT_key'])
 
 # A sample Pushover notification - Working 100%
 apobj.add("pover://" + secrets["Pushover_key"])
@@ -24,11 +24,11 @@ apobj.add("apprise tgram://" + secrets["Telegram_key"] + "/")
 # A sample Microsoft Teams notification  - Working 100% -> "General" Channel
 apobj.add("msteams://" + secrets["Teams_key"])
 
-# A sample Join notification  - Working 100%
-apobj.add("join://" + secrets["Join_key"])
+# A sample Join notification  - I no longer use this tool, it needs Android
+#apobj.add("join://" + secrets["Join_key"])
 
-# A simple Pushbulet notification  - Working 100% (Android ONLY)
-apobj.add("pbul://" + secrets["Pushbullet_key"])
+# A simple Pushbulet notification  - I no longer use this tool, it needs Android
+#apobj.add("pbul://" + secrets["Pushbullet_key"])
 
 # A simple Techulus Push notification - Works but I get 3 messages
 apobj.add("push:///" + secrets["Techulus_key"] + "/")
@@ -37,16 +37,16 @@ apobj.add("push:///" + secrets["Techulus_key"] + "/")
 apobj.add("pushed://" + secrets["Pushed_key"])
 
 # A simple PushSafer notification - Working 100%
-apobj.add("psafers://" + secrets["PushSafer_key"])
+#apobj.add("psafers://" + secrets["PushSafer_key"])
 
 # A simple Mac Desktop notification - Working 100%
-apobj.add("macosx://")
+#apobj.add("macosx://")
 
 # A simple Spontit notification - Working 100%
-apobj.add("spontit://" + secrets["Spontit_key"] + "/")
+#apobj.add("spontit://" + secrets["Spontit_key"] + "/")
 
 # A simple LaMetric notification - TEST THIS!
-apobj.add("lametric://" + secrets["LaMetric_key"])
+#apobj.add("lametric://" + secrets["LaMetric_key"])
 
 
 # Then notify these services any time you desire. The below would
