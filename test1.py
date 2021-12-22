90627764abb12c1f205be4246230d91abc2df078
Matt Packwood <mattpackwood@gmail.com>
2021-12-22 12:24:24 -0500

    Update test1.py
    More testing

--- test1.py
+++ test1.py
@@ -20,8 +20,8 @@ apobj = apprise.Apprise()
 # A sample Pushover notification - Working 100%
 #X apobj.add("pover://" + secrets["Pushover_key"])
 
-# A sample Telegram notification - to errors but nothing coming through
-#X apobj.add("apprise tgram://" + secrets["Telegram_key"] + "/")
+# A sample Telegram notification - Working 100%
+#X apobj.add("apprise tgram://" + secrets["Telegram_key"])
 
 # A sample Microsoft Teams notification  - Working 100% -> "General" Channel
 #X apobj.add("msteams://" + secrets["Teams_key"])
@@ -35,8 +35,8 @@ apobj = apprise.Apprise()
 # A simple PushSafer notification - Working 100%
 #X apobj.add("psafers://" + secrets["PushSafer_key"])
 
-# A simple Mac Desktop notification - Working 100%
-#X pobj.add("macosx://")
+# A simple Mac Desktop notification - Broken
+apobj.add("macosx://")
 
 # A simple Spontit notification - App broken??
 #Y apobj.add("spontit://" + secrets["Spontit_key"] + "/")
@@ -44,8 +44,8 @@ apobj = apprise.Apprise()
 # A simple LaMetric notification - Working 100%
 #X apobj.add("lametric://" + secrets["LaMetric_key"])
 
-# A simple Notica notification - Does not seem to work
-apobj.add("notica://" + secrets["Notify_key"])
+# A simple Notica notification - Working 100%
+#X apobj.add("notica://" + secrets["Notify_key"])
 
 # A simple Popcornnotify notification - Works 100%
 #X apobj.add("popcorn:///" + secrets["Popcornnotify_key"] +"/12483462166")
@@ -53,10 +53,10 @@ apobj.add("notica://" + secrets["Notify_key"])
 # A simple AWS SNS notification - Working 100%!
 #X apobj.add("sns:///" + secrets["SNS_key"] +"/12483462166")
 
-# A simple Twist notification - Works but throws errors
-#Y apobj.add("twist://" + secrets["Twist_key"])
+# A simple Twist notification - Works 100%
+#X apobj.add("twist://" + secrets["Twist_key"])
 
-# A simple Reddit notification - DOES NOT WORK!!!
+# A simple Reddit notification - DOES NOT WORK!!! Issue filed in GitHub
 #apobj.add("reddit://" + secrets["Reddit_key"])
 
 # A sample Join notification  - I no longer use this tool, it needs Android
@@ -66,7 +66,7 @@ apobj.add("notica://" + secrets["Notify_key"])
 #apobj.add("pbul://" + secrets["Pushbullet_key"])
 
 # A sample Join notification  - I no longer use this tool, it needs Android
-apobj.add("twilio://" + secrets["Twilio_Account_SID"] + secrets["Twilio_Auth_Token"] + "@12482189476/1243462166")
+#apobj.add("twilio://" + secrets["Twilio_Account_SID"] + secrets["Twilio_Auth_Token"] + "@12482189476/1243462166")
 
 # Then notify these services any timeyou desire. The below would
 # notify all of the services loaded into our Apprise object.
